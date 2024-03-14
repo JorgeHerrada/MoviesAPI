84acb92313dc52c47e029509ab4cd900ee7d8876
JorgeHerrada <jorge.herrada.28@gmail.com>
2024-03-14 13:17:50 -0600

    Configure Startup.cs

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -2,5 +2,38 @@
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
